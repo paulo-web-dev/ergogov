1540a95af9529940684ea986d9555f2acd494d1f
paulo-web-dev <paulosergioorfanelli@gmail.com>
2026-05-29 18:32:32 -0300

    Ava

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>ergo.gov — Sistema de Gestão Ergonômica</title>
+    <title>Avalia.One — Sistema de Gestão Ergonômica</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">

211f354ec01933b2899d8b0ac920a9f688a9d71c
paulo-web-dev <paulosergioorfanelli@gmail.com>
2026-06-12 10:10:05 -0300

    Veri

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,5 +13,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         '/api/asaas/cartao',
+        '/pesquisa/arp/*',
+        '/formulario/cad/respostas/arp',
     ];
 }
